b0400b517e901dd7b3479c01bff767d09566a93e
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-02-06 20:03:06 +1100

    Working on displaying documentation

---
+++ tasks/documentation.js
@@ -0,0 +1,34 @@
+/* global require, module, Buffer */
+
+var through  = require('through2'),
+	gulputil = require('gulp-util'),
+	util     = require('./utils.js'),
+	PluginError    = gulputil.PluginError;
+
+const PLUGIN_NAME = 'gulp-ractive-foundation-documentation';
+
+function documentation(options) {
+	if (!options) {
+		options = {};
+	}
+
+	var stream = through.obj(function (file, enc, callback) {
+		if (file.isStream()) {
+			this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+			return callback();
+		}
+
+		util.documentation(file.history[0], options)
+			.then(function(contents) {
+				file.contents = new Buffer(contents);
+
+				this.push(file);
+
+				callback();
+			}.bind(this));
+	});
+
+	return stream;
+}
+
+module.exports = documentation;
