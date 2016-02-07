9a64bbee5e97222520811c9051237cd334668f9e
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-02-07 20:58:43 +1100

    Exposing documentation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var component = require('./tasks/component.js');
-var template  = require('./tasks/template.js');
-var plugin    = require('./tasks/plugin.js');
+var component    = require('./tasks/component.js');
+var template     = require('./tasks/template.js');
+var plugin       = require('./tasks/plugin.js');
+var documentation = require('./tasks/documentation.js');
 
 var exports;
-exports.component = component;
-exports.template  = template;
-exports.plugin    = plugin;
+exports.component     = component;
+exports.template      = template;
+exports.plugin        = plugin;
+exports.documentation = documentation;
