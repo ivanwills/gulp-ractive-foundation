0d74a3af3e39af79e0a7eefa2150401d557b3ecc
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-02-07 19:59:28 +1100

    Changed code so it's easer to see gulpfile.js working

--- src/components/gulp-test/components/gulp-sub/gulp-sub.js
+++ src/components/gulp-test/components/gulp-sub/gulp-sub.js
@@ -1,5 +1,5 @@
 /* global Ractive */
 Ractive.components['gulp-test'].extend({
-	template: Ractive.components['gulp-test'].defaults.templates['gulp-sub'],
+	template: Ractive.defaults.templates['gulp-sub'],
 	isolated: true
 });
